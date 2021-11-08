61b7d6b313d6d6500c76e35002967af96ab1ab93
DubovinaDj <49763302+DubovinaDj@users.noreply.github.com>
2021-11-08 16:33:10 +0100

    Update createNFT.sol

--- Task2/createNFT.sol
+++ Task2/createNFT.sol
@@ -1,5 +1,6 @@
-https://ropsten.etherscan.io/address/0x8f9de2150953b9b1f293b226038c51e7c66bde9a#code
 
+//Ropsten:
+https://ropsten.etherscan.io/address/0x8f9de2150953b9b1f293b226038c51e7c66bde9a#code
 
 // SPDX-License-Identifier: MIT
 

3ff6694c570670ac27f9dab423530ae8dce92962
DubovinaDj <49763302+DubovinaDj@users.noreply.github.com>
2021-11-08 16:30:50 +0100

    Create createNFT.sol

---
+++ Task2/createNFT.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DonateAndTakeNFT is ERC721, Ownable {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("Gift NTF", "DNTF") {}
+
+    function mint(address to)
+        public returns (uint256)
+    {
+        uint256 newItemId = tokenCounter;
+        tokenCounter = tokenCounter + 1;
+        _mint(to, newItemId);
+        return newItemId;
+    }
+    
+}
